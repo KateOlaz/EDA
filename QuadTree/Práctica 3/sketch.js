259d06c081f65d4c4a823568dc0d624e0e68dd9b
KateOlaz <49486445+KateOlaz@users.noreply.github.com>
2020-10-04 09:43:07 -0500

    Add files via upload

---
+++ QuadTree/Práctica 3/sketch.js
@@ -0,0 +1,107 @@
+/*
+let qt;
+let count = 0;
+function setup() 
+{
+    createCanvas (400 ,400) ;
+    // centre point and half of width and height
+    let boundary = new Rectangle (200 ,200 ,200 ,200) ;
+    // each leave just could have 4 elements
+    qt = new QuadTree ( boundary , 4) ;
+    console .log (qt);
+    for(let i=0; i < 100; i++) 
+    {
+        let p = new Point(Math.random() * 400, Math.random() * 400) ;
+        qt.insert(p);
+    }
+    background(0) ;
+    qt.show() ;
+}
+
+let qt;
+let count = 0;
+function setup(){
+    createCanvas(400 ,400); //Se crea un elemento de lienzo y se establece las dimensiones del mismo en píxeles
+    let boundary = new Rectangle(200 ,200 ,200 ,200) ;
+    qt = new QuadTree(boundary,4) ;
+}
+
+function draw()
+{
+    background(0);
+    if(mouseIsPressed)
+    {
+        for (let i = 0; i < 1; i ++) {
+            let m = new Point(mouseX + random(-5,5), mouseY + random(-5,5));
+            qt.insert(m)
+        }
+    }
+    background(0);
+    qt.show();
+}
+
+
+let qt;
+let count = 0;
+function setup()
+{
+    createCanvas(400 ,400) ;
+    let boundary = new Rectangle(200 ,200 ,200 ,200) ;
+    qt = new QuadTree(boundary, 4) ;
+    console.log(qt);
+    for(let i =0; i < 1100; i ++) 
+    {
+        let p = new Point(Math.random()*400, Math.random()* 400) ;
+        qt.insert(p);
+    }
+    background(0);
+    qt.show();
+    stroke(0,255,0);
+    rectMode(CENTER);
+    let range = new Rectangle(random(200),random(200),random(50),random(50))
+    rect(range.x, range.y, range.w*2 , range.h *2) ;
+    let points = [];
+    qt.query(range, points);
+    for(let p of points)
+    {
+        strokeWeight(4);
+        point(p.x,p.y);
+    }
+    console.log(count);
+    
+}*/
+
+
+
+let qt;
+let count = 0;
+function setup()
+{
+    createCanvas(400 ,400) ;
+    let boundary = new Rectangle(200 ,200 ,200 ,200) ;
+    qt = new QuadTree(boundary, 4) ;
+    console.log(qt);
+    for(let i=0; i < 100; i++) 
+    {
+        let p = new Point( Math.random() * 400 , Math.random() * 400) ;
+        qt.insert(p);
+    }
+    background(0);
+    qt.show();
+}
+function draw()
+{
+    background(0);
+    qt.show();
+    stroke(0,255,0);
+    rectMode(CENTER);
+    let range = new Rectangle( mouseX ,mouseY ,50 ,50)
+    rect(range.x, range.y, range.w*2 , range.h*2);
+    let points = [];
+    qt.query(range , points);
+    for(let p of points)
+    {
+        strokeWeight (4) ;
+        point (p.x, p.y);
+    }
+}
